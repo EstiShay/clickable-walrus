767842ab49f889b2572bf86a4e20f4911e315d49
Beth Hansen and Esti Shay <esti@>
2017-07-03 11:44:55 -0700

    add clickable walrus image with toggle functionality.

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,26 @@
+// $(document).ready(function() {
+//   jQuery("h1").click(function() {
+//     alert("This is a header.");
+//   });
+//
+//   jQuery("p").click(function() {
+//     alert("This is a paragraph.");
+//   });
+//
+//   jQuery("img").click(function() {
+//     alert("This is an image.");
+//   });
+// });
+
 $(document).ready(function() {
-  jQuery("h1").click(function() {
-    alert("This is a header.");
+  $("p").click(function() {
+    $("img").show();
+    });
   });
 
-  jQuery("p").click(function() {
-    alert("This is a paragraph.");
-  });
-
-  jQuery("img").click(function() {
-    alert("This is an image.");
+$(document).ready(function() {
+  $("p").click(function() {
+    $(".walrus-showing").toggle();
+    $(".walrus-hidden").toggle();
   });
 });
